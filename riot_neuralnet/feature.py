2b74400d099b73a4fa985bcaf0b3d5068dc51950
angelicaperez37 <angelica.perez37@gmail.com>
2017-10-25 16:41:15 -0400

    Extract feature extraction from main tdnn module

---
+++ riot_neuralnet/feature.py
@@ -0,0 +1,12 @@
+from skimage import color, io
+from skimage.feature import hog
+
+
+class Feature:
+
+    def extractFeatureVector(self, imageFile):
+        image = io.imread(imageFile)
+        image.resize((64,64))
+        image = color.rgb2gray(image)
+        featureVector, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualise=True) #, transform_sqrt=True, feature_vector=False)
+        return hog_image
