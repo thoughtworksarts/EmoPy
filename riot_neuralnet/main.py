756a91b5f2dae552cbeab794624ff2b024a3f356
angelicaperez37 <angelica.perez37@gmail.com>
2017-10-25 17:06:04 -0400

    Extract data prep functions

--- riot_neuralnet/main.py
+++ riot_neuralnet/main.py
@@ -3,29 +3,37 @@ import random
 from feature import Feature
 from tdnn import TDNN
 
-def main():
-
+def prepare_training_data(sample_images):
     feature = Feature()
+
     x_temp = list()
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000001.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000002.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000003.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000004.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000005.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000006.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000007.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000008.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000009.png')]])
-    x_temp.append([[feature.extractFeatureVector('../images/S502_001_00000010.png')]])
+    for image in sample_images:
+        x_temp.append([[feature.extractFeatureVector(image)]])
 
     X_train = np.array(x_temp)
+    return X_train
+
+def prepare_training_labels(count):
+    return np.array([random.uniform(0.8, 1)] * count)
+
+
+def main():
+
+    sample_images = list()
+    for i in range(1,11):
+        sample_images.append('../images/S502_001_000000%02d.png' % i)
+
+
+    feature = Feature()
+    X_train = prepare_training_data(sample_images)
     print (X_train.shape)
-    y_train = np.array([random.uniform(0.8,1)]*10)
+    y_train = prepare_training_labels(10)
     X_test = np.array([[[feature.extractFeatureVector('../images/S502_001_00000001.png')]]])
     print(X_test.shape)
     y_test = np.array([1])
 
     tdnn = TDNN(verbose=True)
     tdnn.train(X_train, y_train, X_test, y_test)
+
 
 main()
