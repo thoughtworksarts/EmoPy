d76da64b2e9c8e9750d88ef8705073c95db926ea
Angelica Perez <aperez@thoughtworks.com>
2017-12-04 13:55:26 -0500

    [Angelica] Initialize FERModel class that will function as top layer model

--- fer/fermodel.py
+++ fer/fermodel.py
@@ -1,11 +1,22 @@
 
 
 class FERModel:
-    def __init__(self, target_emotions):
-        self.target_emotions = target_emotions
+    POSSIBLE_EMOTIONS = ['anger', 'fear', 'calm', 'sad', 'happy', 'surprise']
 
-    def initialize_model(self):
+    def __init__(self, target_emotions):
+        if not self._emotions_are_valid(target_emotions):
+            raise ValueError('Target emotions must be subset of %s.' % self.POSSIBLE_EMOTIONS)
+        self.target_emotions = target_emotions
+        self._initialize_model()
+
+    def _initialize_model(self):
+        print('Initializing FER model for target emotions: %s' % self.target_emotions)
+
+    def train(self):
         pass
 
     def predict(self, images):
         pass
+
+    def _emotions_are_valid(self, emotions):
+        return set(emotions).issubset(set(self.POSSIBLE_EMOTIONS))
---
+++ fer/fermodel_test.py
@@ -0,0 +1,4 @@
+from fermodel import FERModel
+
+target_emotions = ['anger', 'fear', 'calm', 'sad', 'happy', 'surprise']
+model = FERModel(target_emotions)
