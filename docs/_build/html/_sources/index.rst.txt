4d5547936f694838bc3f18c27286140d2211c95d
Angelica Perez <aperez@thoughtworks.com>
2017-12-20 12:02:59 -0800

    Update Sphinx docs library name to emopy.

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -1,9 +1,9 @@
-.. FERPython documentation master file, created by
+.. EMOPY documentation master file, created by
    sphinx-quickstart on Tue Nov 28 16:37:23 2017.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to FERPython's documentation!
+Welcome to EMOPY's documentation!
 =====================================
 
 .. toctree::
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# FERPython documentation build configuration file, created by
+# EMOPY documentation build configuration file, created by
 # sphinx-quickstart on Tue Nov 28 16:37:23 2017.
 #
 # This file is execfile()d with the current directory set to its
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'FERPython'
+project = 'EMOPY'
 copyright = '2017, AP'
 author = 'AP'
 
@@ -119,7 +119,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'FERPythondoc'
+htmlhelp_basename = 'EMOPYdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -146,7 +146,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'FERPython.tex', 'FERPython Documentation',
+    (master_doc, 'EMOPY.tex', 'EMOPY Documentation',
      'AP', 'manual'),
 ]
 
@@ -156,7 +156,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ferpython', 'FERPython Documentation',
+    (master_doc, 'emopy', 'EMOPY Documentation',
      [author], 1)
 ]
 
@@ -167,8 +167,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'FERPython', 'FERPython Documentation',
-     author, 'FERPython', 'One line description of project.',
+    (master_doc, 'EMOPY', 'EMOPY Documentation',
+     author, 'EMOPY', 'One line description of project.',
      'Miscellaneous'),
 ]
 
