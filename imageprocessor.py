287e72fc54cd36b6461c2f8155fa675023e53fb9
Angelica Perez <aperez@thoughtworks.com>
2018-01-25 15:14:33 -0500

    Extract method

--- imageprocessor.py
+++ imageprocessor.py
@@ -26,18 +26,10 @@ class ImageProcessor:
 
         images = list()
         for raw_image in self.images:
-            image = raw_image
-            if not self.time_series:
-                image = cv2.resize(raw_image, self.target_dimensions, interpolation=cv2.INTER_LINEAR)
-            if self.time_series:
-                image = list()
-                for slice in raw_image:
-                    image.append(cv2.resize(slice, self.target_dimensions, interpolation=cv2.INTER_LINEAR))
-            elif self.rgb:
+            image = self._resize_image_sample(raw_image)
+            if self.rgb:
                 image = np.array([image, image, image]).reshape((self.target_dimensions[0], self.target_dimensions[1], 3))
-
             images.append(image)
-
         if self.augment_data:
             data_gen = ImageDataGenerator(rotation_range=180)
             data_gen.fit(images)   # TODO: functionality: send data_gen new image set to feature extractor
@@ -47,3 +39,12 @@ class ImageProcessor:
         print('Training data extraction runtime - ' + str(end-start))
 
         return np.array(images)
+
+    def _resize_image_sample(self, raw_image):
+        if not self.time_series:
+            return cv2.resize(raw_image, self.target_dimensions, interpolation=cv2.INTER_LINEAR)
+        if self.time_series:
+            sample = list()
+            for slice in raw_image:
+                sample.append(cv2.resize(slice, self.target_dimensions, interpolation=cv2.INTER_LINEAR))
+            return sample
