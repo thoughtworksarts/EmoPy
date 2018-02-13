3fc25687719f17505f736f0049c71c4c93fa4eb3
Puneetha Pai B P <puneethp@thoughtworks.com>
2018-02-14 01:13:37 +0530

    Add validation logic in DataGenerator and appropriate tests

--- data_generator.py
+++ data_generator.py
@@ -7,6 +7,9 @@ K.set_image_dim_ordering('th')
 
 class DataGenerator:
     def __init__(self, time_delay=None):
+        self.time_delay = time_delay
+        self.images = None
+        self.labels = None
         self.config_augmentation(time_delay=time_delay)
 
     def config_augmentation(self, zca_whitening=False, rotation_angle=90, shift_range=0.2, horizontal_flip=True,
@@ -22,16 +25,35 @@ class DataGenerator:
         return self
 
     def fit(self, images, labels):
+        self._validate(images, labels)
         self.images = images
         self.labels = labels
         self.data_gen.fit(self.images)
         return self
 
-    def get_next_batch(self, batch_size=10):
-        for images, labels in self.data_gen.flow(self.images, self.labels, batch_size=batch_size):
-            samples, pixels, width, height = images.shape
-            images = images.reshape(samples, width, height)
+    def get_next_batch(self, batch_size=10, target_dimension=None):
+        self._check_model_has_been_fit()
+        for images, labels in self.data_gen.flow(self.images, self.labels, batch_size=batch_size,
+                                                 target_dimension=target_dimension):
             return images, labels
 
     def generate(self, target_dimensions=None, batch_size=10):
+        self._check_model_has_been_fit()
         return self.data_gen.flow(self.images, self.labels, batch_size=batch_size, target_dimension=target_dimensions)
+
+    def _validate(self, images, labels):
+        if len(images) != len(labels):
+            raise ValueError("Samples are not labeled properly")
+        if images.ndim < 4:
+            raise ValueError("Channel Axis should have vale")
+        if self.time_delay:
+            if images.ndim != 5:
+                raise ValueError("Time_delay parameter was set but Images say otherwise")
+        if images.ndim == 5 and images.shape[1] != self.time_delay:
+            raise ValueError("Images have time axis length {given} "
+                             "but time_delay parameter was set to {set}"
+                             .format(given=images.shape[1], set=self.time_delay))
+
+    def _check_model_has_been_fit(self):
+        if self.images is None or self.labels is None:
+            raise ValueError("Model is not fit to any data set yet")
---
+++ tests/unittests/src/data_generator_test.py
@@ -0,0 +1,65 @@
+import unittest
+import numpy as np
+
+from data_generator import DataGenerator
+
+
+class DataGeneratorTest(unittest.TestCase):
+    def test_should_produce_augmented_samples_given_batch_size(self):
+        images = np.random.rand(20, 64, 64, 1)
+        labels = np.random.rand(20)
+        generator = DataGenerator().fit(images, labels)
+        batch_size = 10
+        batch, _ = generator.get_next_batch(batch_size)
+        self.assertEqual(batch.shape[0], batch_size)
+        self.assertEqual(images.shape[1:], batch.shape[1:])
+
+    def test_should_resize_images_to_given_target_dimension(self):
+        images = np.random.rand(20, 64, 64, 3)
+        labels = np.random.rand(20)
+        generator = DataGenerator().fit(images, labels)
+        batch, _ = generator.get_next_batch(10, target_dimension=(28, 28))
+        self.assertEqual(batch.shape, (10, 28, 28, 3))
+
+    def test_should_raise_error_when_labels_and_samples_are_mis_matched(self):
+        images = np.random.rand(20, 64, 64)
+        with self.assertRaises(ValueError) as e:
+            DataGenerator().fit(images, [1])
+        self.assertEqual("Samples are not labeled properly", str(e.exception))
+
+    def test_should_raise_error_when_channel_axis_is_not_present(self):
+        images = np.random.rand(20, 64, 64)
+        labels = np.random.rand(20)
+        with self.assertRaises(ValueError) as e:
+            DataGenerator().fit(images, labels)
+        self.assertEqual("Channel Axis should have vale", str(e.exception))
+
+    def test_should_raise_error_when_time_delay_parameter_is_set_and_input_is_simple_images(self):
+        images = np.random.rand(10, 64, 64, 3)
+        labels = np.random.rand(10)
+        with self.assertRaises(ValueError) as e:
+            DataGenerator(time_delay=4).fit(images, labels)
+        self.assertEqual("Time_delay parameter was set but Images say otherwise", str(e.exception))
+
+    def test_should_raise_error_when_time_delay_was_not_set_and_input_is_time_series(self):
+        images = np.random.rand(10, 2, 64, 64, 3)
+        labels = np.random.rand(10)
+        with self.assertRaises(ValueError) as e:
+            DataGenerator().fit(images, labels)
+        self.assertEqual("Images have time axis length 2 but time_delay parameter was set to None", str(e.exception))
+
+    def test_should_raise_error_if_time_delay_is_not_matching_input_time_axis(self):
+        images = np.random.rand(10, 4, 64, 64, 3)
+        labels = np.random.rand(10)
+        with self.assertRaises(ValueError) as e:
+            DataGenerator(time_delay=5).fit(images, labels)
+        self.assertEqual("Images have time axis length 4 but time_delay parameter was set to 5", str(e.exception))
+
+    def test_should_raise_error_if_model_not_fit_to_data_yet(self):
+        with self.assertRaises(ValueError) as a:
+            DataGenerator(time_delay=5).get_next_batch()
+        with self.assertRaises(ValueError) as b:
+            DataGenerator(time_delay=5).generate()
+
+        self.assertEqual("Model is not fit to any data set yet", str(a.exception))
+        self.assertEqual("Model is not fit to any data set yet", str(b.exception))
