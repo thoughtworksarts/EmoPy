ea7ba6ed0f235c3e7f0d0b6440ea7a7cfcf449b3
ThoughtWorker <thoughtworker@thoughtworker.local>
2018-10-16 14:02:22 -0500

    Configure MANIFEST.in to only include enough images to make the examples run

--- setup.py
+++ setup.py
@@ -13,11 +13,6 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url="https://github.com/thoughtworksarts/EmoPy",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     include_package_data=True,
     python_requires='>=3.6.3,<3.7',
     install_requires=[
